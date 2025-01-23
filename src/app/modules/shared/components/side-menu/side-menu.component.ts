a47a7bce838481edbd5dd9317bacf2c7eab0a13e
Felix Nunez <felix.nunez@vass.com.mx>
2025-01-23 09:17:09 -0700

    Trabajando en panel administrativo

---
+++ src/app/modules/shared/components/side-menu/side-menu.component.ts
@@ -0,0 +1,19 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, input, output } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'side-menu',
+  standalone: true,
+  imports: [CommonModule, RouterLink, RouterLinkActive],
+  templateUrl: './side-menu.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SideMenuComponent { 
+
+  isAuthenticated = input(false);
+
+  onSignOut = output();
+  onSignIn = output();
+
+}
